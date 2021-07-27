8410a4ea3a8fb02b2d269930930cf6fff6c7a508
s-plotnikov <seryoga.22@mail.ru>
2021-07-27 22:33:12 +0700

    Initial commit

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+    
+        $('.partners__slider').slick({
+            infinite: true,
+            slidesToShow: 4,
+            arrows: true,
+            prevArrow: '<button class="slick-arrow slick-prev"><img src="images/slider/arrow-left.svg" alt=""></button>',
+            nextArrow: '<button class="slick-arrow slick-next"><img src="images/slider/arrow-right.svg" alt=""></button>',
+        });
+
+})
